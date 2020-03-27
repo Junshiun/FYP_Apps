6fbef575a28ddb1b7fc2f36426d2e273fb60b0ab
Junshiun <55488934+Junshiun@users.noreply.github.com>
2020-03-27 18:45:56 +0800

    Schedule Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -39,6 +40,7 @@ import { Ng5SliderModule } from 'ng5-slider';
     MatButtonModule,
     MatButtonToggleModule,
     Ng5SliderModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -6,7 +6,7 @@ import * as firebase from 'firebase/app';
 
 import { Router } from '@angular/router';
 
-import { Options, LabelType, ChangeContext, PointerType} from 'ng5-slider';
+import { Options, LabelType, ChangeContext } from 'ng5-slider';
 
 @Component({
   selector: 'app-schedule',
@@ -17,42 +17,80 @@ import { Options, LabelType, ChangeContext, PointerType} from 'ng5-slider';
 export class ScheduleComponent implements OnInit {
 
   constructor(db: AngularFireDatabase, private router: Router) {
+    this.GetValue_Firebase();
   }
 
-  value = 2;
-  halo = 0;
+  hide = [true, true, true, true, true, true, true];
+  hide02 = [true, true, true, true, true, true, true];
   DayInWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  OperationHourStartTimeArray: any[] = [];
+  OperationHourEndTimeArray: any[] = [];
   PeakHourStartTimeArray: any[] = [];
+  PeakHourEndTimeArray: any[] = [];
+  values = 23;
   options: Options = {
     floor: 0,
     ceil: 24,
     scale: 1,
     step: 1,
     showTicks: true,
+    showSelectionBar: true,
+    getLegend: (value: number): string => {
+      return value + '';
+    },
 
    translate: (value: number, label: LabelType): string => {
       switch (label) {
         case LabelType.Low:
-          return value + ':00';
-        case LabelType.High:
-          return value + ':00';
+          return '';
         default:
-          return '' + value;
+          return '';
       }
     }
   };
 
+  toggleDisplay(day: number) {
+    this.hide[day] = !this.hide[day];
+  }
+
+  toggleDisplay02(day: number) {
+    this.hide02[day] = !this.hide02[day];
+  }
+
   ngOnInit() {
-    this.GetValue_Firebase();
     this.myprint();
   }
 
   GetValue_Firebase() {
+    this. OperationHourStartTimeArray.splice(0, this. OperationHourStartTimeArray.length);
+    for (let i = 0; i < 7; i++) {
+      firebase.database().ref('Schedule/Day/' + this.DayInWeek[i] + '/Operation_Hour/Start_Time').on('value', (snapshot) => {
+        console.log(snapshot.val());
+        this. OperationHourStartTimeArray.splice(i, 1, snapshot.val());
+      });
+    }
+
+    this. OperationHourEndTimeArray.splice(0, this. OperationHourEndTimeArray.length);
+    for (let i = 0; i < 7; i++) {
+      firebase.database().ref('Schedule/Day/' + this.DayInWeek[i] + '/Operation_Hour/End_Time').on('value', (snapshot) => {
+        console.log(snapshot.val());
+        this. OperationHourEndTimeArray.splice(i, 1, snapshot.val());
+      });
+    }
+
     this.PeakHourStartTimeArray.splice(0, this.PeakHourStartTimeArray.length);
     for (let i = 0; i < 7; i++) {
       firebase.database().ref('Schedule/Day/' + this.DayInWeek[i] + '/Peak_Hour/Start_Time').on('value', (snapshot) => {
         console.log(snapshot.val());
         this.PeakHourStartTimeArray.splice(i, 1, snapshot.val());
+      });
+    }
+
+    this.PeakHourEndTimeArray.splice(0, this.PeakHourEndTimeArray.length);
+    for (let i = 0; i < 7; i++) {
+      firebase.database().ref('Schedule/Day/' + this.DayInWeek[i] + '/Peak_Hour/End_Time').on('value', (snapshot) => {
+        console.log(snapshot.val());
+        this.PeakHourEndTimeArray.splice(i, 1, snapshot.val());
       });
     }
     /*
@@ -92,15 +130,22 @@ export class ScheduleComponent implements OnInit {
     firebase.database().ref('Schedule/Day/' + day + '/Peak_Hour/End_Time').set(peak.highValue);
   }
 
+  Day_OperationHour_Update(day: string, peak: ChangeContext) {
+    console.log(day);
+    console.log(peak.value);
+    console.log(peak.highValue);
+    firebase.database().ref('Schedule/Day/' + day + '/Operation_Hour/Start_Time').set(peak.value);
+    firebase.database().ref('Schedule/Day/' + day + '/Operation_Hour/End_Time').set(peak.highValue);
+  }
+
   myprint() {
     this.timeout();
-    this.halo = this.PeakHourStartTimeArray[0];
     console.log(this.PeakHourStartTimeArray);
   }
 
   timeout() {
     // tslint:disable-next-line: only-arrow-functions
     setTimeout(function() {
-    }, 2000);
+    }, 3000);
   }
 }
