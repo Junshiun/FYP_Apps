0260347f1406303c6f4c1761a81a750d960f049a
Junshiun <55488934+Junshiun@users.noreply.github.com>
2020-04-01 14:16:36 +0800

    Promise

--- src/app/new-chart/new-chart.component.ts
+++ src/app/new-chart/new-chart.component.ts
@@ -18,33 +18,73 @@ import {MatDatepickerInputEvent} from '@angular/material/datepicker';
 export class NewChartComponent implements OnInit {
   Linechart: Chart;
 
-  dataArray: any[] = [];
-  xArray: any[] = [];
+  dataArray: any[] = [[{}], [{}], [{}]];
   wholedate: string[] = ['order'];
   monthsArray: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   date: string;
   month: string;
   year: string;
 
+  Current: any;
+
+  CurrentYear: any;
+  CurrentMonth: any;
+  CurrentDate: any;
+
+  NodeArray1: any[] = [1, 2];
+  NodeArray2: any[] = [3, 4];
+  NodeArray: any[] = [this.NodeArray2, this.NodeArray1];
+
   constructor(public db: AngularFireDatabase) {
-    const leadsRef =  firebase.database().ref('Data/2020/3/29/Area_01/Node_01/');
-    // tslint:disable-next-line: only-arrow-functions
-    leadsRef.on('value', (snapshot) => {
-      this.dataArray.splice(0, this.dataArray.length);
-      this.xArray.splice(0, this.dataArray.length);
-        // tslint:disable-next-line: only-arrow-functions
-      snapshot.forEach((childSnapshot) => {
-        const childData = childSnapshot.val();
-        const childData02 = childSnapshot.key;
-        this.dataArray.push(childData);
-        this.xArray.push(childData02);
-      });
-      this.mychart(this.dataArray, this.xArray);
-    });
   }
 
   ngOnInit() {
-    /*this.Upload();*/
+    this.thisFunction();
+  }
+
+  /*
+  test01() {
+    console.log('Entered first function');
+    return new Promise(resolve => {
+      firebase.database().ref('Current_Time').on('value', (snapshot) => {
+        this.CurrentYear = snapshot.val().Year;
+        this.CurrentMonth = snapshot.val().Month;
+        this.CurrentDate = snapshot.val().Date;
+      });
+      // tslint:disable-next-line: only-arrow-functions
+      setTimeout(function() {
+      resolve('\t\t This is first promise');
+      console.log('Returned first promise');
+      }, 2000);
+    });
+  }
+
+  test02() {
+    console.log('Entered second function');
+    console.log(this.CurrentYear);
+    return new Promise(resolve => {
+        // tslint:disable-next-line: only-arrow-functions
+        setTimeout(function() {
+        resolve('\t\t This is second promise');
+        console.log('Returned second promise');
+        }, 2000);
+    });
+  }
+
+  async test03() {
+    console.log('async function called');
+    const firstpromise = await this.test01();
+    console.log('After awaiting for 2 seconds,' + 'the promise returned from first function is:');
+    console.log(firstpromise);
+    const secondpromise = await this.test02();
+    console.log('After awaiting for 4 seconds, the' + 'promise returned from second function is:');
+    console.log(secondpromise);
+  }*/
+
+  async thisFunction() {
+    const firstpromise = await this.Promise01();
+    const secondpromise = await this.Promise02();
+    const thirdpromise = await this.Promise03(this.dataArray);
   }
 
   addEvent(type: string, event: MatDatepickerInputEvent<Date>) {
@@ -60,46 +100,126 @@ export class NewChartComponent implements OnInit {
 
   Upload() {
     for (let i = 0; i <= 30; i++) {
-      firebase.database().ref('Data/2020/3/29/Area_01/Node_01/' + i).set(1.92 + (i / 30));
+      firebase.database().ref('Data/2020/3/31/Node_1/' + i).set(1.92 + (0.01 * i));
+      firebase.database().ref('Data/2020/3/31/Node_2/' + i).set(1.34 + (0.02 * i));
     }
   }
 
-  mychart(key, axis) {
+  Promise01() {
+    return new Promise(resolve => {
+      this.GetFromFirebase01();
+      // tslint:disable-next-line: only-arrow-functions
+      setTimeout(function() {
+        resolve();
+        }, 2000);
+    });
+  }
+
+  Promise02() {
+    return new Promise(resolve => {
+      this.GetFromFirebase02();
+      console.log(this.dataArray);
+      // tslint:disable-next-line: only-arrow-functions
+      setTimeout(function() {
+        resolve(this.dataArray);
+        }, 1000);
+    });
+  }
+
+  Promise03(secondpromise) {
+    return new Promise(resolve => {
+      this.mychart(secondpromise);
+      // tslint:disable-next-line: only-arrow-functions
+      setTimeout(function() {
+        resolve();
+        }, 1000);
+    });
+  }
+
+  GetFromFirebase01() {
+    firebase.database().ref('Current_Time').on('value', (snapshot) => {
+      this.CurrentYear = snapshot.val().Year;
+      this.CurrentMonth = snapshot.val().Month;
+      this.CurrentDate = snapshot.val().Date;
+    });
+  }
+
+  GetFromFirebase02() {
+    for ( let i = 0; i < 3; i++) {
+      this.Current = ('Data/' + this.CurrentYear + '/' + this.CurrentMonth + '/' + this.CurrentDate + '/Node_' + (i + 1));
+      const leadsRef =  firebase.database().ref(this.Current);
+      // tslint:disable-next-line: only-arrow-functions
+      leadsRef.on('value', (snapshot) => {
+        this.dataArray[i].splice(0, this.dataArray[i].length);
+          // tslint:disable-next-line: only-arrow-functions
+        snapshot.forEach((childSnapshot) => {
+          let childData = childSnapshot.val();
+          if (childData === 0) {
+            childData = 'null';
+          }
+          const childData02 = childSnapshot.key;
+          this.dataArray[i].push({x: childData02, y: childData});
+        });
+        /*this.mychart(this.dataArray);*/
+      });
+    }
+  }
+
+  mychart(key) {
     this.Linechart = new Chart('linechart', {
-      type: 'line',
+      type: 'scatter',
       plugins: [ChartAnnotation],
       data: {
-        labels: axis,
-        datasets: [{
-            data: key,
-            label: 'Concentration level',
-            borderColor: '#3e95cd',
-            fill: false
-          }
+        datasets: [
+          {
+            label: 'Node 1',
+            data: key[0].slice(-180),
+            showLine: true,
+            fill: false,
+            borderColor: 'rgba(0, 200, 0, 1)',
+            lineTension: 0
+          },
+          {
+            label: 'Node 2',
+            data: key[1].slice(-180),
+            showLine: true,
+            fill: false,
+            borderColor: 'rgba(46, 134, 178)',
+            lineTension: 0
+          },
+          {
+            label: 'Node 3',
+            data: key[2].slice(180),
+            showLine: true,
+            fill: false,
+            borderColor: 'rgba(206, 17, 17)',
+            lineTension: 0
+          },
         ]
       },
       options: {
-        responsive: true,
-        title: {
-          display: true,
-          text: 'Chlorine Gas Concentration (ppm)'
+        tooltips: {
+          mode: 'index',
+          intersect: false,
         },
-        plugins: {
-          annotation: {
-              annotations: [
-                  {
-                    type: 'line',
-                    id: 'hLine',
-                    mode: 'horizontal',
-                    scaleID: 'y-axis-0',
-                    value: 1.88,  // data-value at which the line is drawn
-                    borderWidth: 4,
-                    borderColor: '#3e95cd',
-                  }
-              ]
-          }
+        hover: {
+          mode: 'nearest',
+          intersect: true
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
         },
       }
     });
+  }
+
+  timeout() {
+    // tslint:disable-next-line: only-arrow-functions
+    setTimeout(function() {
+    }, 5000);
   }
 }
